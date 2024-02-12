698277d0c4993acde81989e19c7530e200fd1359
jean519 <129189905+jean519@users.noreply.github.com>
2024-02-12 16:37:35 +0100

    component

--- src/app/componat-test/componat-test.component.ts
+++ src/app/componat-test/componat-test.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import user from '../user-profile.js';
 
 @Component({
   selector: 'app-componat-test',
   templateUrl: './componat-test.component.html',
-  styleUrl: './componat-test.component.css'
+  styleUrl: './componat-test.component.css',
 })
-export class ComponatTestComponent {
+export class ComponatTestComponent implements OnInit {
+  //variable
+  name: string = 'Jean-Mathieu';
+  //Data-Binding
+  imageSrc: string = 'https://via.placeholder.com/350x150';
+  //EventBinding
+  sayHello(): void {
+    alert('bonjour');
+  }
+  userName: string = user.name;
+  userFirstName: string = user.firstName;
+  userAge: number = user.age;
+  userQuote: string | null | undefined = user.quote;
+  userPhoto: string = user.photo;
 
+  //useEffect
+  ngOnInit(): void {}
 }
---
+++ src/app/user-profile.ts
@@ -0,0 +1,9 @@
+const user = {
+  name: 'Doe',
+  firstName: 'John',
+  age: 25,
+  quote: '',
+  photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+};
+
+export default user;
